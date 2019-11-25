2d106f6ee435042811b122fcccfe1bb18169f167
liuxiao <18389677071@163.com>
2019-11-26 01:51:20 +0800

    1. build project 2. add icons

---
+++ Swift假面舞会/Swift假面舞会/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Swift假面舞会
+//
+//  Created by suckerl on 2019/11/26.
+//  Copyright © 2019 suckerl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.orange
+        
+    }
+
+
+}
+
